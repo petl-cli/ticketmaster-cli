3ee7c0b97c27db974c3cdfd07b3b1f0eeefb8ef5
rishimantri795 <106453647+rishimantri795@users.noreply.github.com>
2026-05-03 17:47:10 -0400

    release: v0.1.3

--- commands/feedback.go
+++ commands/feedback.go
@@ -58,7 +58,7 @@ func runFeedback(cmd *cobra.Command, args []string) error {
 	caller := telemetry.DetectCaller()
 
 	id, err := feedback.Submit(context.Background(), feedbackEndpoint, feedbackToken, feedback.Payload{
-		CLIVersion:     "0.1.2",
+		CLIVersion:     "0.1.3",
 		Message:        message,
 		CommandContext: feedbackFlags.about,
 		AgentType:      caller.AgentType,
